37b5c0825666d241cf338a95b83f377a2c0927a4
Juliane-C <juliane.cristinara97@gmail.com>
2020-10-28 17:40:30 -0300

    adicionei array

--- cipher.js
+++ cipher.js
@@ -1,5 +1,21 @@
+//Esse documento é para colocar a lógica do projeto (de cifrar e descrifrar);
+const alfabeto=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
 const cipher = {
   // ...
 };
 
 export default cipher;
+
+function name(params) {
+  
+}
+
+if (condition) {
+  
+} else {
+  
+}
+
+console.log();
