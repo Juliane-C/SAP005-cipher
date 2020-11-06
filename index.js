5490d300729ddca6efb717ab84567ec226371ad5
Juliane-C <juliane.cristinara97@gmail.com>
2020-11-06 18:31:27 -0300

    Tentativa de fazer o DOM

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 //Esse documento é para fazer o DOM
 import cipher from './cipher.js';
 
+
+function cliquec() {
+    let c= prompt("Insira sua mensagem:") + document.getElementById(buttonc)
+}
+
+function cliqued() {
+    let d= prompt("Insira sua mensagem:") + document.getElementById(buttond)
+}
+
+
+
 console.log(cipher);
