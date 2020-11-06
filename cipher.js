16b2d05f6cf48140c6cc2fb1927e6aec184372c7
Juliane-C <juliane.cristinara97@gmail.com>
2020-11-06 18:32:58 -0300

    Tentativa de fazer o cipher

--- cipher.js
+++ cipher.js
@@ -1,9 +1,21 @@
 //Esse documento é para colocar a lógica do projeto (de cifrar e descrifrar);
-const alfabeto=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let c; //variável que armazenara a palavra para cifrar
+let d; //armazenará a palavra para decifrar
+const alfabeto= [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const ascii = 65; // ver se vou usar a variável ou o número
+let offset; //vai armazenar o valor em number do offset
 
+  const cipher = {
+    //colocar as funções de cifrar e decifrar
+    function encode(c, offset){
 
-const cipher = {
-  // ...
+    }
+    
+    function decode(d, offset){
+
+    }
+
+  
 };
 
 export default cipher;
