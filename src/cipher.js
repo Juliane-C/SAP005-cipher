29fabb1651f48b19885500713f9653aa631ea2b7
Juliane-C <juliane.cristinara97@gmail.com>
2020-11-07 17:02:21 -0300

    Atualizando o projeto para a versão final

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,15 @@
 //Esse documento é para colocar a lógica do projeto (de cifrar e descrifrar);
-let c; //variável que armazenara a palavra para cifrar
-let d; //armazenará a palavra para decifrar
+let cifra; //variável que armazenara a palavra para cifrar
+let decifra; //armazenará a palavra para decifrar
 const alfabeto= [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const ascii = 65; // ver se vou usar a variável ou o número
+const ascii = 65; //ver se vou usar a variável ou o número
 let offset; //vai armazenar o valor em number do offset
 
   const cipher = {
     //colocar as funções de cifrar e decifrar
-    function encode(c, offset){
-
-    }
-    
-    function decode(d, offset){
-
-    }
-
   
 };
 
 export default cipher;
-
-function name(params) {
-  
-}
-
-if (condition) {
-  
-} else {
-  
-}
 
 console.log();
