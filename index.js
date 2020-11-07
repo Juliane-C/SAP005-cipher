f2d71ae1a178ccc05f511d0c928f395230506227
Juliane-C <juliane.cristinara97@gmail.com>
2020-11-07 17:02:21 -0300

    Atualizando o projeto para a versão final

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //Esse documento é para fazer o DOM
 import cipher from './cipher.js';
-
-
-function cliquec() {
-    let c= prompt("Insira sua mensagem:") + document.getElementById(buttonc)
+function cliqueC(){
+    let cifra = new MouseEvent("click", prompt("Insira a sua mensagem:").value);
+    for(let c=1;c<=1; c++){
+        Number(prompt("Insira o número do offset"));
+    }
+    //o .value pega o movimento feito no hmtl,pra executar a função aqui no JS
+    //Tá fluindo o objetivo de com o clique do usuário acionar o console \o/
+    //Testar o for aqui pra ver se repete o prompt, mas para armazenar o offset (OK)
 }
+document.getElementById("buttonc").addEventListener('click', cliqueC);
 
-function cliqued() {
-    let d= prompt("Insira sua mensagem:") + document.getElementById(buttond)
+function cliqueD(){
+    let decifra = new MouseEvent("click", prompt("Insira a sua mensagem:").value);
+    for(let d=1;d<=1; d++){
+        Number(prompt("Insira o número do offset"));
+    }    
 }
-
-
+document.getElementById("buttond").addEventListener('click', cliqueD);
 
 console.log(cipher);
